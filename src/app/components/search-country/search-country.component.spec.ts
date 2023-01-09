4622ab0da0f39f37174f2d46a40fa7970a5a7abb
unknown <jca1983@gmail.com>
2023-01-09 15:25:49 -0300

    '12e12e2e12e21'

--- src/app/components/search-country/search-country.component.spec.ts
+++ src/app/components/search-country/search-country.component.spec.ts
@@ -1,11 +1,11 @@
 import { fakeAsync, TestBed, tick } from '@angular/core/testing';
-import { CountriesService } from 'src/app/services/countries.service';
-import { SearchCountryComponent } from './search-country.component';
-import { COUNTRIES } from 'src/app/mocks/countries.mock';
-import { of } from 'rxjs';
-import { switchMap, takeWhile } from 'rxjs/operators';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { HttpClient } from '@angular/common/http';
+import { takeWhile } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { SearchCountryComponent } from './search-country.component';
+import { CountriesService } from 'src/app/services/countries.service';
+import { COUNTRIES } from 'src/app/mocks/countries.mock';
 
 describe('SearchCountryComponent', () => {
   let component: SearchCountryComponent,
@@ -21,20 +21,26 @@ describe('SearchCountryComponent', () => {
 
     http = TestBed.inject(HttpClient);
     httpMock = TestBed.inject(HttpTestingController);
-    component = new SearchCountryComponent(serv);
     serv = TestBed.inject(CountriesService);
+    component = new SearchCountryComponent(serv);
   });
 
   afterEach(() => component.ngOnDestroy());
 
   it('should create', () => expect(component).toBeTruthy());
 
-  it('should test ngOnInit', () => {
-    let init = spyOn(component, 'ngOnInit');
-    init();
-    expect(init).toHaveBeenCalled();
-  });
-
+  it('should call getCountries method of countriesService', fakeAsync(() => {
+    component.ngOnInit();
+    serv._countries$.next(COUNTRIES);
+    component.countries = COUNTRIES;
+    serv.getCountries();
+    serv.getCountries()
+      .pipe(takeWhile(() => component.unsub))
+      .subscribe((c: any) => {
+        component.countries = c;
+        expect(component.countries).toEqual(c);
+      });
+  }));
 
   it('should test insertPost(body)', async () => {
     let req = await httpMock.expectOne('http://localhost:3000/countries');
@@ -42,52 +48,28 @@ describe('SearchCountryComponent', () => {
     req.flush(req);
   });
 
-
-
-  it('should call getCountries method of countriesService', () => {
-    of(COUNTRIES).subscribe((c: any) => {
-      serv._countries$.next(c);
-      component.countries = c;
-      serv.getCountries();
-    });
-    serv.getCountries()
-    .pipe(takeWhile(() => component.unsub))
-    .subscribe((c: any) => {
-      expect(component.unsub).toBeTruthy();
-      component.countries = c;
-      expect(component.countries).toEqual(c);
-    });
-
-    component.ngOnInit();
-
+  it('should test ngOnInit', () => {
+    let init = spyOn(component, 'ngOnInit');
+    init();
+    expect(init).toHaveBeenCalled();
   });
 
-
   it('should set selectedCountry to an empty array if the search term is less than two characters',
-  fakeAsync(async () => {
+  fakeAsync(() => {
     component.searchCountry({ target: { value: 'a' } });
     tick(501);
     component.selectedCountry = [];
     expect(component.selectedCountry).toEqual([]);
   }));
 
-
   it('should set selectedCountry to an empty array if the search term is more than two characters',
-  fakeAsync(async () => {
+  fakeAsync(() => {
     of(COUNTRIES).subscribe((c: any) => component.countries = c);
-
     component.searchCountry({ target: { value: 'braz' } });
     tick(101);
-
     let spyGetCountry = spyOn(component, 'getCountry');
-
-    of(spyGetCountry('braz')).subscribe((c: any) => {
-      component.countries = c;
-      expect(component.countries).toEqual(c);
-    });
-
+    of(spyGetCountry('braz')).subscribe((c: any) => component.countries = c);
     component.selectedCountry = [{ "country": "Brazil", "id": "BR" }];
-
     expect(spyGetCountry).toHaveBeenCalledWith('braz');
     expect(component.getCountry).toHaveBeenCalledWith('braz');
     expect(component.selectedCountry).toEqual([{ "country": "Brazil", "id": "BR" }]);
